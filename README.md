a3837380ef1867ff57950f9c27391e04f2c8a0eb
segodimo <segodimo@gmail.com>
2024-06-24 10:52:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Ex do meu primeiro plugin para Nvim e Lazy
+
+https://github.com/segodimor2d2/explugin-nvim
+
+## Instalação
+
+"segodimor2d2/formatxt"
+
+### Crie um arquivo .lua em .config/nvim/lua/plugins/qualquerNome.lua qualquer 
+
+#### -- Adicione o seguinte código para instalar via Lazy
+
+---
+```lua
+return {
+  "segodimor2d2/formatxt",
+  config = true,
+}
+```
+> para executar Lazy: leader-l
+> para instalar o plugin: leader-i
+
+### Testes:
+
+
+line1
+  line2
+
+  line3
+
+
+line4
+      line5
+
+line6
+  line7
+
+
+Gerenciamento da Complexidade
+A arquitetura de software é vista como uma ferramenta essencial para gerenciar a complexidade inerente aos sistemas de software
+Sistemas sem uma arquitetura adequada podem se tornar difíceis de entender
+manter e evoluir à medida que crescem
+
+Compreensão Global do Sistema
+Uma arquitetura bem definida fornece uma visão global do sistema, permitindo que desenvolvedores e equipes compreendam a estrutura e as interações entre os diferentes componentes
+
+
+Gerenciamento da Complexidade: A arquitetura de software|é vista como uma ferramenta essencial para gerenciar a complexidade inerente aos sistemas de software. Sistemas sem uma arquitetura adequada podem se tornar difíceis de entender, manter e evoluir à medida que crescem.
+Compreensão Global do Sistema: Uma arquitetura bem definida fornece uma visão global do sistema, permitindo que desenvolvedores e equipes compreendam a estrutura e as interações entre os diferentes componentes.
+
+
+
+
+
+Facilitação da Manutenção: Uma arquitetura adequada facilita a manutenção do sistema ao longo do tempo. Isso é crucial, uma vez que a maioria dos sistemas de software passa por várias fases de evolução e atualização.
+
+Escalabilidade: A arquitetura influencia a capacidade do sistema de escalar para atender a requisitos crescentes. Uma boa arquitetura é projetada para suportar o crescimento e a adição de novos recursos sem comprometer a estabilidade.
+Adaptabilidade e Flexibilidade: A capacidade de adaptar-se a mudanças nos requisitos de negócios e tecnológicos é crucial. A arquitetura desempenha um papel importante na facilitação dessa adaptabilidade, permitindo que o sistema evolua sem grandes reestruturações.
+Redução de Riscos: Uma arquitetura bem planejada ajuda a reduzir riscos associados ao desenvolvimento, garantindo que decisões importantes sobre design e estrutura sejam tomadas conscientemente.
+Facilitação da Colaboração: Uma compreensão clara da arquitetura do sistema facilita a colaboração entre membros da equipe. Cada desenvolvedor pode trabalhar em sua parte do sistema, entendendo como ela se encaixa no todo.
+Sustentabilidade a Longo Prazo: A arquitetura contribui para a sustentabilidade a longo prazo do software, garantindo que ele permaneça viável e fácil de manter ao longo de várias iterações e atualizações.
+
+
+
+
+
---
+++ lua/formatxt/init.lua
@@ -0,0 +1,136 @@
+local M = {}
+
+M.emptylines = function()
+	local getBuffer = vim.api.nvim_get_current_buf()
+
+	local _, srow, scol = unpack(vim.fn.getpos("v"))
+	local _, erow, ecol = unpack(vim.fn.getpos("."))
+	local lini = -1
+	local lfin = -1
+
+	local lines = {}
+
+	if vim.fn.mode() == "V" then
+		if srow > erow then
+			lini, lfin = erow, srow
+		else
+			lini, lfin = srow, erow
+		end
+		lines = vim.api.nvim_buf_get_lines(0, lini - 1, lfin, true)
+	end
+
+	-- local outLines = vim.split(outTxt, "\n", true)
+
+	-- Divide a string outTxt em linhas
+	-- local outLines = {}
+	-- for line in outTxt:gmatch("[\r\n]+") do
+	-- 	table.insert(outLines, line)
+	-- end
+
+	local listout = {}
+	for i = 1, #lines do
+		if lines[i] ~= "" then
+			table.insert(listout, lines[i])
+		end
+	end
+
+	vim.api.nvim_buf_set_lines(getBuffer, lini - 1, lfin, false, listout)
+end
+
+M.addPrefixLines = function()
+	local getBuffer = vim.api.nvim_get_current_buf()
+
+	local _, srow, scol = unpack(vim.fn.getpos("v"))
+	local _, erow, ecol = unpack(vim.fn.getpos("."))
+	local lini = -1
+	local lfin = -1
+
+	local lines = {}
+
+	if vim.fn.mode() == "V" then
+		if srow > erow then
+			lini, lfin = erow, srow
+		else
+			lini, lfin = srow, erow
+		end
+		lines = vim.api.nvim_buf_get_lines(0, lini - 1, lfin, true)
+	end
+
+	local listout = {}
+	for i = 1, #lines do
+		if lines[i] ~= "" then
+			local resAddPrefix = string.gsub(lines[i], "^([ \t]*)", "%1- ")
+			table.insert(listout, resAddPrefix)
+		end
+	end
+
+	vim.api.nvim_buf_set_lines(getBuffer, lini - 1, lfin, false, listout)
+	--
+	-- -- Pegar o range de linhas atualmente selecionadas no visual mode
+	-- local start_line, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
+	-- local end_line, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
+	--
+	-- -- Iterar sobre cada linha no range e adicionar o prefixo
+	-- for line_num = start_line, end_line do
+	--   local line = vim.fn.getline(line_num)
+	--   if line ~= "" then
+	--     vim.fn.setline(line_num, "- " .. line)
+	--   end
+	-- end
+end
+
+M.gsubCharForBLline = function()
+	local getBuffer = vim.api.nvim_get_current_buf()
+
+	local _, srow, scol = unpack(vim.fn.getpos("v"))
+	local _, erow, ecol = unpack(vim.fn.getpos("."))
+	local lini = -1
+	local lfin = -1
+
+	local lines = {}
+
+	if vim.fn.mode() == "V" then
+		if srow > erow then
+			lini, lfin = erow, srow
+		else
+			lini, lfin = srow, erow
+		end
+		lines = vim.api.nvim_buf_get_lines(0, lini - 1, lfin, true)
+	end
+
+	local listToString = table.concat(lines, "|")
+
+	local addLineBreak = string.gsub(listToString, "[,:.|]", "\n")
+
+	local result = {}
+	local delimiter = "\n"
+	for match in (addLineBreak .. delimiter):gmatch("(.-)" .. delimiter) do
+		table.insert(result, match)
+	end
+
+	local listout = {}
+	for i = 1, #result do
+		if result[i] ~= "" then
+			local stringTrimmed = result[i]:gsub("^%s*", "")
+			table.insert(listout, stringTrimmed)
+		end
+	end
+
+	vim.api.nvim_buf_set_lines(getBuffer, lini - 1, lfin, false, listout)
+end
+
+M.setup = function()
+	vim.keymap.set("v", "<leader>fd", function()
+		M.emptylines()
+	end, { desc = "DelEmptyLines" })
+
+	vim.keymap.set("v", "<leader>fp", function()
+		M.addPrefixLines()
+	end, { desc = "AddPrefix" })
+
+	vim.keymap.set("v", "<leader>fb", function()
+		M.gsubCharForBLline()
+	end, { desc = "BracketLine" })
+end
+
+return M
